47f94bb0d6b6fe0c8707b07da8516133d16249eb
anthony-murè <tony.mure99@gmail.com>
2025-08-04 17:00:19 +0200

    [add]milestone-2(create-folder-data&&create-file-dataPosts.js)

---
+++ data/dataPosts.js
@@ -0,0 +1,40 @@
+
+//array dei posts
+
+const posts = [
+   {
+    titolo:'Ciambellone',
+    contenuto: 'Ricetta Ciambellone' ,
+    immagine:'/imgs/ciambellone.jpeg',
+    tags:['Farina','Uova','Zucchero'],
+   },
+   {
+    titolo:'Cracker Barbabietola',
+    contenuto: 'Ricetta Cracker alla Barbabietola' ,
+    immagine:'/imgs/cracker_barbabietola.jpeg',
+    tags:['Cracker','Barbabietola','Viola'],
+   },
+   {
+    titolo:'Pane Fritto',
+    contenuto: 'Ricetta Pane Fritto Dolce' ,
+    immagine:'/imgs/pane_fritto_dolce.jpeg',
+    tags:['Pane','Fritto','Dolce'],
+   },
+   {
+    titolo:'Pasta alla Barbabietola',
+    contenuto: 'Ricetta Pasta alla Barbabietola' ,
+    immagine:'/imgs/pasta_barbabietola.jpeg',
+    tags:['Pasta','Barbabietola','Viola'],
+   },
+   {
+    titolo:'Torta Paesana',
+    contenuto: 'Ricetta della Torta Paesana' ,
+    immagine:'/imgs/torta_paesana.jpeg',
+    tags:['Torta','Paesana','Buona'],
+   }
+
+];
+
+//esporto l'array dei posts
+
+module.exports = posts;
